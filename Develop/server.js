2e5e5fcc55c8fc5c68a9a3973c853de7f18f51b2
IamPopov <ivantsar@gmail.com>
2020-01-22 12:16:36 -0700

    added basic paths  for server

--- Develop/server.js
+++ Develop/server.js
@@ -1,15 +1,32 @@
-const express = require("express");
-var app = express()
+// Dependencies
+// =============================================================
+var express = require("express");
+var path = require("path");
+
+// Sets up the Express App
+// =============================================================
+var app = express();
 var PORT = 3000;
 
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
+var notes = [];
+
+// Basic route that sends the user first to the AJAX Page
 app.get("/", function(req, res) {
-    res.send("index.html");
+    res.sendFile(path.join(__dirname, "public/index.html"));
   });
 
-app.get("/:", (req, res) => {
-    
-})
+app.get("/notes", function(req, res) {
+    res.sendFile(path.join(__dirname, "public/notes.html"));
+  });
+
+// Displays all notes
+app.get("/api/notes", function(req, res) {
+  return res.json(notes);
+});
 
 // Listener
 // ===========================================================
