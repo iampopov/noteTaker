ca9aee48a1c27a122ad5652663832a9bdc252eb3
IamPopov <ivantsar@gmail.com>
2020-01-22 21:14:31 -0700

    read from db.json

--- Develop/server.js
+++ Develop/server.js
@@ -1,33 +1,92 @@
 // Dependencies
 // =============================================================
-var express = require("express");
-var path = require("path");
+const express = require("express");
+const path = require("path");
 
 // Sets up the Express App
 // =============================================================
-var app = express();
-var PORT = 3000;
+const app = express();
+const PORT = 3000;
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static("public"));
 
-var notes = [];
-
+const fs = require('fs')
+const db = path.join(__dirname, "db/db.json");
+let notes; 
+//reads from db.json
+fs.readFile(db, 'utf8', (err, data) => {
+  if (err) throw err;
+  notes = JSON.parse(data);
+});
+// Routes
+// =============================================================
 // Basic route that sends the user first to the AJAX Page
-app.get("/", function(req, res) {
-    res.sendFile(path.join(__dirname, "public/index.html"));
-  });
 
 app.get("/notes", function(req, res) {
-    res.sendFile(path.join(__dirname, "public/notes.html"));
+  res.sendFile(path.join(__dirname, "/public/notes.html"));
+});
+
+app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "./public/index.html"));
   });
+
 
 // Displays all notes
 app.get("/api/notes", function(req, res) {
   return res.json(notes);
 });
 
+
+
+//Display single note
+app.get("/api/notes/:note", function(req, res) {
+  const chosen = req.params.note;
+
+  //console.log(chosen);
+
+  for (let i=0; i<notes.length; i++) {
+    if (chosen === notes[i].noteName) {
+      return res.json(notes[i]);
+    }
+  }
+  return res.json(false);
+});
+
+//Create new note
+app.post("/api/notes", function(req, res) {
+  let newNote = req.body;
+
+  let count = 0;
+  
+
+  newNote.id = count++;
+
+  console.log(newNote);
+  
+  fs.appendFile(db, 'newNote', err => {
+    if (err) throw err;
+    console.log('saved!')
+  })
+  //notes.push(newNote);
+  res.json(newNote);
+})
+//api/note/:id
+
+//const chosen = req.params.id;
+
+//splice(id)
+//delete .splice() at certain location
+app.delete("/api/notes/:id", function(req, res) {
+  const deletingNote = req.params.id;
+  console.log(deletingNote)
+
+  //note.spice(deletingNote)
+
+
+})
 // Listener
 // ===========================================================
 app.listen(PORT, function() {
